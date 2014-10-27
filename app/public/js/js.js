0e4dff96390c13a77390b5574a5107708d856de7
Tyler Dinner <ttante@me.com>
2014-10-26 23:44:45 -0400

    ajax working!

--- app/public/js/js.js
+++ app/public/js/js.js
@@ -1,3 +1,24 @@
 window.onload = function() {
 	console.log('js.js loaded!');
 }
+
+$(document).ready(function(e) {
+	$("form[ajax=true]").submit(function(e) {
+		
+		e.preventDefault();
+
+		var form_data = $(this).serialize();
+		var form_url = $(this).attr("action");
+		var form_method = $(this).attr("method").toUpperCase();
+
+		$.ajax({
+			url: form_url,
+			type: form_method,
+			data: form_data,
+			cache: false,
+			success: function(returnhtml) {
+				$("#result").html(returnhtml);
+			}
+		});
+	});
+});

7030e0ff7c152f076ebee011a0e2c3ee340b5d8f
Tyler Dinner <tyler@Tylers-MacBook-Pro.local>
2015-02-14 16:20:34 -0500

    cleaned up ajax files

--- app/public/js/js.js
+++ app/public/js/js.js
@@ -1,49 +1,5 @@
 $(document).ready(function(e) {
 
-
-
-
-	$("form#login").submit(function(e) {
-		console.log('submit fired');
-		e.preventDefault();
-
-		var form_data = $(this).serialize();
-		var form_url = $(this).attr("action");
-		var form_method = $(this).attr("method").toUpperCase();
-
-		$.ajax({
-			url: form_url,
-			type: form_method,
-			data: form_data,
-			cache: false,
-			success: function(returnhtml) {
-				console.log('Succesfully added user!');
-			}
-		});
-	});
-
-
-	$("form#createUser").submit(function(e) {
-		console.log('submit fired');
-		e.preventDefault();
-
-		var form_data = $(this).serialize();
-		var form_url = $(this).attr("action");
-		var form_method = $(this).attr("method").toUpperCase();
-
-		$.ajax({
-			url: form_url,
-			type: form_method,
-			data: form_data,
-			cache: false,
-			success: function(returnhtml) {
-				console.log('Succesfully added user!');
-			}
-		});
-	});
-
-
-
 	$('.login-btn').click(function(e) {
 		e.preventDefault();
 		$('.login-form').css({'display': 'inline-block'});
@@ -53,11 +9,6 @@ $(document).ready(function(e) {
 		e.preventDefault();
 		$('.login-form').css({'display': 'none'});
 	});
-
-
-
-
-
 
 
 });

6b49dced7536afa9c6299bea57ba8cec82b7eab8
Tyler Dinner <tyler@Tylers-MacBook-Pro.local>
2015-03-28 11:55:24 -0400

    data loading into ckeditor!

--- app/public/js/createPost.js
+++ app/public/js/createPost.js
@@ -1,23 +1,25 @@
 (function($){
 
-	$(document).ready(function () { 
-		var wysiwyg_args = {
+  $(document).ready(function () { 
+	var wysiwyg_args = {
 		    "autoresize": true,
 		    "resize": false,
 //		    "imageUpload": "\/admin\/img\/add",
 		    "bodyClass": "_-wsyiwyg CKEditorInputArea CKEditorGeneratedContent RTEGeneratedContent"
 		};
 
-		CKEDITOR.replace(document.getElementById('content'),wysiwyg_args);
+		var editor = CKEDITOR.replace(document.getElementById('content'),wysiwyg_args);
 		console.log('stuff',document.getElementById('content'),CKEDITOR); 
 		
+		var data = editor.getData();
+		console.log('data' + data);
 		
 	}); 
 
-	("form#createUser").submit(function(e) {
-		var data = CKEDITOR.instances.editor1.getData();
-		console.log('data');
-	});
+	//$("form#createPost").submit(function(e) {
+//		var data = CKEDITOR.instances.editor1.getData();
+//		alert('data: ' +  data);
+//	});
 
 
 })(jQuery);

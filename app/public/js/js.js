11694fed006e95cdb18db22664a97025cbca163f
ttante <tylerdinner@gmail.com>
2016-04-06 00:17:28 -0400

    new nav working

--- app/public/js/js.js
+++ app/public/js/js.js
@@ -1,14 +1,33 @@
-$(document).ready(function(e) {
+(function($){
+	$(document).ready(function(e) {
 
-	$('.login-btn').click(function(e) {
-		e.preventDefault();
-		$('.login-form').css({'display': 'inline-block'});
-	});
+		$('.login-btn').click(function(e) {
+			e.preventDefault();
+			$('.login-form').css({'display': 'inline-block'});
+		});
 
-	$('.close-btn').click(function(e) {
-		e.preventDefault();
-		$('.login-form').css({'display': 'none'});
-	});
+		$('.close-btn').click(function(e) {
+			e.preventDefault();
+			$('.login-form').css({'display': 'none'});
+		});
+
+		var scrollHeight = $('#body-wrap').scrollTop();
+		var scrollNewHeight;
+		$('#body-wrap').bind('scroll', function(){
+			scrollNewHeight = $(this).scrollTop();
+			if(scrollHeight < scrollNewHeight){
+				console.log('going down');
+				$('.customNav').addClass('small');
+			}else{
+				console.log('going up');
+				$('.customNav').removeClass('small');
+			}
+
+			scrollHeight = scrollNewHeight;
+			// console.log('this.scrollTop(): ');
+			// console.log($(this).scrollTop());
+		});
 
 
-});
+	}); 
+})(jQuery);
